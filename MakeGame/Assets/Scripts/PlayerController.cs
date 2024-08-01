3c0c9c454a58049ceba7fb48bf3b3e50781f4fff
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2024-08-01 10:55:36 +0900

    Vector
    벡터에 관해서,
    normalized, magnitude

--- MakeGame/Assets/Scripts/PlayerController.cs
+++ MakeGame/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 1. 위치 벡터
+// 2. 방향 벡터
+struct MyVector
+{
+
+    public float x;
+    public float y;
+    public float z;
+    
+    // 피타고라스
+    //          +
+    //     +    +
+    // +--------+
+    public float magnitude { get { return Mathf.Sqrt(x * x + y * y + z * z); } }
+
+    // normalized 단위벡터
+    public MyVector normalized { get { return new MyVector(x / magnitude, y / magnitude, z / magnitude); } }
+
+    public MyVector(float x, float y, float z) { this.x = x; this.y = y; this.z = z; }
+
+    public static MyVector operator + (MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static MyVector operator - (MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static MyVector operator *(MyVector a, float d)
+    {
+        return new MyVector(a.x * d, a.y * d, a.z * d);
+    }
+
+}
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
@@ -9,13 +45,27 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        
+        MyVector to = new MyVector(10.0f, 0.0f, 0.0f);
+        MyVector from = new MyVector(5.0f, 0.0f, 0.0f);
+        MyVector dir = to - from; //(-5.0f,0.0f,0.0f)
+
+        dir = dir.normalized; // (1.0f; 0.0f, 0.0f)
+
+        MyVector newPos = from + dir * speed;
+
+        // 방향 벡터
+            // 1. 거리(크기) 5  magnitude
+            // 2. 실제 방향 ->  normalized
+
     }
     //GameObject(Player)
         // trnaform
         //PlayerController(*)
     void Update()
     {
+        //transform.position.magnitude;
+        //transform.position.normalized;
+
         // Local -> Global;
         // TransformDirection
         
