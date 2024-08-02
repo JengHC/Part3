47fa1d1330cd37a7ea3d33b824f749cbd0e732eb
HeeCheol Jeong <70684380+JengHC@users.noreply.github.com>
2024-08-02 11:28:33 +0900

    Prefab
     Nested prefab, prefab variant 등등

--- MakeGame/Assets/Scripts/PlayerController.cs
+++ MakeGame/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+class Tank
+{
+    public float speed = 10.0f;
+    
+}
+
+class FastTank : Tank
+{
+
+}
+
+
+class Player
+{
+
+}
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,6 +29,16 @@ public class PlayerController : MonoBehaviour
     {
         ProjectManager.input.KeyAction -= OnKeyboard;
         ProjectManager.input.KeyAction += OnKeyboard;
+
+        Tank tank1 = new Tank(); // Tank Instance를 만든다.
+        tank1.speed = 11.0f;
+        Tank tank2 = new Tank(); // Tank Instance를 만든다.
+        tank2.speed = 21.0f;
+        Tank tank3 = new Tank(); // Tank Instance를 만든다.
+        Tank tank4 = new Tank(); // Tank Instance를 만든다.
+        Tank tank5 = new Tank(); // Tank Instance를 만든다.
+
+
 
     }
 
